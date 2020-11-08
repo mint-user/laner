c91bb109ec9262c70538e7787e6d5587a300fced
AVP <alexander.pronin@nasc-dev.com>
2020-11-08 20:25:59 +0300

    added admin user

--- core_models/index.js
+++ core_models/index.js
@@ -1,13 +1,40 @@
-var Sequelize = require('sequelize')
+const Sequelize = require('sequelize')
 const db = require('../db')
 
-var Account = db.define('account', {
+const Workgroup = db.define('workgroup', {
+    title: { type: Sequelize.STRING, unique: true, allowNull: false },
+    alias: { type: Sequelize.STRING, unique: true, allowNull: false }
+})
+
+const Account = db.define('account', {
     login: { type: Sequelize.STRING, unique: true, allowNull: false },
     password: { type: Sequelize.STRING, allowNull: false},
     jwtToken: {type: Sequelize.STRING}
-   })
+})
 
-// Account.sync({force: true})
-Account.sync()
+Account.belongsTo(Workgroup)
+Workgroup.hasOne(Account)
+
+async function struct(){
+    await Workgroup.sync({force: true})
+    // await Account.belongsTo(Workgroup)
+    // await Workgroup.hasOne(Account)
+    await Account.sync({force: true})
+    await Workgroup.create({ alias: "admin", title: "Admin"})
+    await Account.create({ login: "admin", password: "admin", workgroupId: 1})
+}
+
+struct()
+
+
+
+
+// Account.sync()
+//    .then(() => {
+//        Account.findOne()
+//    })
+
+// check admin existing
+// Account.findOne()
 
 module.exports = Account
