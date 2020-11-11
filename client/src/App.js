03255b25bacf89690fd5a1612a6e3b75887ab127
AVP <alexander.pronin@nasc-dev.com>
2020-11-11 23:20:47 +0300

    created login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,44 @@
-import { Button } from 'react-materialize';
+import { Row, Col, Card, Icon, TextInput, CardPanel, CardTitle, Button, Navbar, NavItem } from 'react-materialize';
+import M from "materialize-css";
 
 function App() {
   return (
-    <div className="container">
-      <Button>
-        Hello1fghdfgd
-      </Button>
-      
+    <div>
+      <Navbar
+        alignLinks="right"
+        brand={<a className="brand-logo" href="#">Logo</a>}
+      >
+        <NavItem>Github</NavItem>
+      </Navbar>
+      <div className="container">
+        <Row>
+          <Col s={12} m={4} offset={'m4'}>
+            <Card>
+              <form action="http://localhost:3000/sign_in">
+                <Row>
+                  <TextInput
+                    validate={true}
+                    id="email"
+                    email={true}
+                    s={12}
+                    label="Email"
+                  />
+                </Row>
+                <Row>
+                  <TextInput
+                    validate={true}
+                    id="password"
+                    password={true}
+                    s={12}
+                    label="Password"
+                  />
+                </Row>
+                <Button type="submit">Login</Button>
+              </form>
+            </Card>
+          </Col>
+        </Row>    
+      </div>
     </div>
   );
 }
