6536d74402b632218f604c46a2ffdb6b6fc664fc
AVP <alexander.pronin@nasc-dev.com>
2020-11-14 23:17:38 +0300

    added auth validation backend+frontend

--- client/src/components/Login/LoginForm.js
+++ client/src/components/Login/LoginForm.js
@@ -1,39 +1,62 @@
-import {React} from 'react'
+import {React, useState} from 'react'
 import { Row, Col, Card, TextInput, Button } from 'react-materialize'
 import axios from "axios"
 
 export const LoginForm = () => {
 
-    axios.create({
-        // baseURL: "http://localhost:5000/",
-        baseURL: "/",
-        responseType: "json"
-      })
+    const [loginValues, setLogin] = useState({login:"", password:""})
 
-      
-    const loginHandler = () => {
-        console.log("send credentials")
+    axios.create({ responseType: "json" })
 
+    axios.interceptors.response.use(response => {
+        return response;
+    }, error => {
+        if (error.response.status === 401) {
+            //place your reentry code
+            // console.log(error.response)
+            return error.response;
+        }
+            return error;
+    });
+
+    const loginHandler = (event) => {
         let userData = axios.post('/sign_in', {
-            login: "admin",
-            password: "admin"
-        }).then(userData => console.log(userData))
+            login: loginValues.login,
+            password: loginValues.password
+        })
+        .then(resp => {
+            console.log(`ANSWER:`, resp)
+            if(resp.status===401){
+                console.error(resp.data)
+            } else {
+                console.log('norm')
+            }
+        })
+        .catch(error => {
+            let errObj = JSON.parse(JSON.stringify(error))
+            console.log("error.response", error.response)
+            console.error(`ANSWER:`, errObj)
+        })
     }
 
     return (
         <div className="container">
+            <p>{JSON.stringify(loginValues)}</p>
             <Row>
                 <Col s={12} m={4} offset={'m4'}>
                     <Card>
                         <Row>
                             <TextInput
                                 validate={true}
-                                id="email"
-                                name="email"
-                                email={true}
+                                id="login"
+                                name="login"
                                 s={12}
-                                label="Email"
-                            />
+                                label="Login"
+                                onChange={(e) => {
+                                    setLogin({...loginValues, login: e.target.value})
+                                }}
+                                value={loginValues.login}
+                                />
                         </Row>
                         <Row>
                             <TextInput
@@ -43,9 +66,16 @@ export const LoginForm = () => {
                                 password={true}
                                 s={12}
                                 label="Password"
+                                onChange={(e) => {
+                                    setLogin({...loginValues, password: e.target.value})
+                                }}
+                                value={loginValues.password}
                             />
                         </Row>
-                        <Button onClick={loginHandler} >
+                        <Button
+                            onClick={loginHandler}
+                            // disabled={ loginValues.login && loginValues.password ? false : true}
+                        >
                             Login
                         </Button>
                     </Card>
--- routes/index.js
+++ routes/index.js
@@ -6,39 +6,41 @@ const Account = require("../core_models");
 const router = Router()
 
 router.post('/sign_in', async (req, res) => {
-  // res.json({"fgdfg":"234"})
-  console.log(req.body)
   let errors = []
-  if(!req.body.login){ errors.push("Login has not been received") }
-  if (!req.body.password) { errors.push("Passwod has not been received") }
-  if(errors.length>0){
-    res
-      .set('content-type','application/json')
-      .status(404)
-      .json({"stat":"fail","errors":errors})  
-  }
-
+  // if (!req.body.login){ errors.push("Login has not been received") }
+  if (!req.body.login){ sendAnswer(res, ["Login has not been received"]) }
+  // if (!req.body.password) { errors.push("Passwod has not been received") }
+  if (!req.body.password) { sendAnswer(res, ["Passwod has not been received"]) }
+  
   const { login, password } = req.body
 
   try {
-    const acc = await Account.findOne({where: {login: login}})
-    console.log(acc)
-    if(!acc){res.status(401).json({"mess":"user not found"})}
-    // const hashedPass = await bcrypt.hash(password, 12)
-    const passMatch = await bcrypt.compare(password, acc.password)
-    if(!passMatch){res.status(401).json({"mess":"wrong passwod"})}
-    const token = jwt.sign(
-      { id: acc.id },
-      "secret",
-      { expiresIn: '1h' }
-      )
-      res.status(200).json({token: token, accountId: acc.id})
-    } catch (error) {
-      console.log(error)
-    res.status(500).json(`Something goes wrong: ${error}`)
-  }
+    acc = await Account.findOne({where: {login: login}})
+    if (acc){
+      const passMatch = await bcrypt.compare(password, acc.password)
+      if(passMatch){
+        token = jwt.sign({ id: acc.id }, "secret", { expiresIn: '1h' })
+      } else {errors.push("wrong passwod")}
+    } else { {errors.push("user not found")} }
+    
+  } catch (error) { errors.push(`Something goes wrong: ${error}`) }
+
+  sendAnswer(res, errors)
 })
 
+function sendAnswer(res, errors){
+  if (errors.length>0) {
+    res
+      .set('content-type','application/json')
+      .status(401)
+      .json({"stat": "fail","errors": errors})  
+  } else {
+    res
+      .set('content-type','application/json')
+      .status(200)
+      .json({token: token, accountId: acc.id})
+  }
+}
 
 
 module.exports = router 
